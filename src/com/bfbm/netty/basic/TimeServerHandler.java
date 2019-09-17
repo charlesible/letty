887cdeaad4707e564118419eb19f476cbc6ba285
成走 <chundong.lcd@alibaba-inc.com>
2019-09-18 01:24:38 +0800

    pretty1

--- src/com/bfbm/netty/basic/TimeServerHandler.java
+++ src/com/bfbm/netty/basic/TimeServerHandler.java
@@ -20,34 +20,31 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
-/**
-
- *
- *
- */
 public class TimeServerHandler extends ChannelHandlerAdapter {
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-	    throws Exception {
-	ByteBuf buf = (ByteBuf) msg;
-	byte[] req = new byte[buf.readableBytes()];
-	buf.readBytes(req);
-	String body = new String(req, "UTF-8");
-	System.out.println("The time server receive order : " + body);
-	String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
-		System.currentTimeMillis()).toString() : "BAD ORDER";
-	ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-	ctx.write(resp);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8").replace("\n", "");
+        System.out.println("The time server receive order : " + body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+                new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        for (int i = 0; i < 1000; i++) {
+            ctx.writeAndFlush(resp);
+        }
+        ctx.channel().close();
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-	ctx.flush();
+        ctx.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-	ctx.close();
+        ctx.close();
     }
 }
--- src/com/bfbm/netty/bio/TimeClient.java
+++ src/com/bfbm/netty/bio/TimeClient.java
@@ -24,24 +24,20 @@ import java.net.Socket;
 
 public class TimeClient {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-
         int port = 8080;
-        if (args != null && args.length > 0) {
 
+        if (args != null && args.length > 0) {
             try {
                 port = Integer.valueOf(args[0]);
             } catch (NumberFormatException e) {
                 // 采用默认值
             }
-
         }
         Socket socket = null;
         BufferedReader in = null;
         PrintWriter out = null;
+
         try {
             socket = new Socket("127.0.0.1", port);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
